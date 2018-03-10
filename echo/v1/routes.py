7e4c4fb907559e34c9eaacdec12ca9bdd394b4b5
Jack <jack@developerjack.com>
2018-03-10 21:08:06 +1100

    swagger_py_codegen generated API implementation

---
+++ echo/v1/routes.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+###
+### DO NOT CHANGE THIS FILE
+### 
+### The code is auto generated, your change will be overwritten by 
+### code generating.
+###
+from __future__ import absolute_import
+
+from .api.echo import Echo
+
+
+routes = [
+    dict(resource=Echo, urls=['/echo'], endpoint='echo'),
+]
