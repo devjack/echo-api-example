10e006436352bda01745f9935921fd1c4ccb1551
Jack <jack@developerjack.com>
2018-03-10 18:19:09 +1100

    Implemented the echo API endpoint in flask

---
+++ api.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({
+        'code': '404',
+        'message': "URL not found"
+    }), 404
+
+@app.route("/echo")
+def hello():
+    echo = request.args.get('echo')
+    response = {
+        'hello': 'world',
+        'echo' : echo
+    }
+    return jsonify(response)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
